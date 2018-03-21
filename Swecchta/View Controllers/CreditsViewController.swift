0e8f711c7885cc2edbeb7c15dcdd37bb6fa312bd
Tejas Upmanyu <tejasupmanyu@gmail.com>
2018-03-21 23:31:00 +0530

    Removed compiler Mach Errors.

--- Swecchta/View Controllers/CreditsViewController.swift
+++ Swecchta/View Controllers/CreditsViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class CreditsViewController: UIViewController, PaperOnboardingDataSource {
+    func onboardingItem(at index: Int) -> OnboardingItemInfo {
+        let backgroundColorOne = UIColor(red: 217/255, green: 72/255, blue: 89/255, alpha: 1)
+        let backgroundColorTwo = UIColor(red: 106/255, green: 166/255, blue: 211/255, alpha: 1)
+        let backgroundColorThree = UIColor(red: 168/255, green: 200/255, blue: 78/255, alpha: 1)
+        let backgroundColorfour = UIColor(red: 220/255, green: 200/255, blue: 178/255, alpha: 1)
+        let backgroundColorfive = UIColor(red: 97/255, green: 198/255, blue: 227/255, alpha: 1)
+        let textColor = UIColor.white
+        let DescriptionColor = UIColor.white
+        let textFont = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        let DescriptionFont = UIFont.systemFont(ofSize: 18, weight: .medium)
+        
+        
+        let one = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Tejas"), title: "Tejas Upmanyu", description: "iOS Developer/ UX & UI", pageIcon: UIImage(), color: backgroundColorOne, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
+        
+        let two =  OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Shabbir"), title: "Shabbir Hussain", description: "Android Developer", pageIcon: UIImage(), color: backgroundColorTwo, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
+        
+        let three =  OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Shivam"), title: "Shivam Bharadwaj", description: "Web Developer", pageIcon: UIImage(), color: backgroundColorThree, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
+        
+        let four =  OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Neha"), title: "Neha Shukla", description: "Shukla Pari", pageIcon: UIImage(), color: backgroundColorfour, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
+        
+        let five = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Suhasini"), title: "Suhasini Singyan", description: "Team Leader", pageIcon: UIImage(), color: backgroundColorfive, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
+        
+        
+        return [one, two, three, four, five][index]
+    }
+    
 
     @IBOutlet weak var creditsView: PaperOnboarding!
     
@@ -30,33 +56,6 @@ class CreditsViewController: UIViewController, PaperOnboardingDataSource {
         return 5
     }
     
-    func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
-        
-        let backgroundColorOne = UIColor(red: 217/255, green: 72/255, blue: 89/255, alpha: 1)
-        let backgroundColorTwo = UIColor(red: 106/255, green: 166/255, blue: 211/255, alpha: 1)
-        let backgroundColorThree = UIColor(red: 168/255, green: 200/255, blue: 78/255, alpha: 1)
-        let backgroundColorfour = UIColor(red: 220/255, green: 200/255, blue: 178/255, alpha: 1)
-        let backgroundColorfive = UIColor(red: 97/255, green: 198/255, blue: 227/255, alpha: 1)
-        let textColor = UIColor.white
-        let DescriptionColor = UIColor.white
-        let textFont = UIFont.systemFont(ofSize: 32, weight: .heavy)
-        let DescriptionFont = UIFont.systemFont(ofSize: 18, weight: .medium)
-        
-        
-        let one = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Tejas"), title: "Tejas Upmanyu", description: "iOS Developer/ UX & UI", pageIcon: UIImage(), color: backgroundColorOne, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
-        
-       let two =  OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Shabbir"), title: "Shabbir Hussain", description: "Android Developer", pageIcon: UIImage(), color: backgroundColorTwo, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
-        
-       let three =  OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Shivam"), title: "Shivam Bharadwaj", description: "Web Developer", pageIcon: UIImage(), color: backgroundColorThree, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
-        
-       let four =  OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Neha"), title: "Neha Shukla", description: "Shukla Pari", pageIcon: UIImage(), color: backgroundColorfour, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
-        
-        let five = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "Suhasini"), title: "Suhasini Singyan", description: "Team Leader", pageIcon: UIImage(), color: backgroundColorfive, titleColor: textColor, descriptionColor: DescriptionColor, titleFont: textFont, descriptionFont: DescriptionFont)
-        
-        
-        return [one, two, three, four, five][index]
-    }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
